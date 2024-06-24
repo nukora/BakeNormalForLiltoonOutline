5bb57edfe4b4e8bbb7777ead11be32a9e887245f
nukora <jp.nukora@gmail.com>
2024-06-24 18:19:02 +0900

    first commit

---
+++ __init__.py
@@ -0,0 +1,42 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+import bpy
+from .operators import BNFLO_OT_bake_normal
+
+bl_info = {
+    "name" : "Bake Normals for lilToon Outline",
+    "author" : "Nukora",
+    "description" : "Bake normals to vertex colors for lilToon outline.",
+    "blender" : (4, 1, 0),
+    "version" : (1, 0, 0),
+    "location" : "View3D > Object",
+    "warning" : "",
+    "category" : "Object"
+}
+
+classes = (
+    BNFLO_OT_bake_normal,
+)
+
+def menu_fn(self, context):
+    self.layout.operator(BNFLO_OT_bake_normal.bl_idname, text="Bake Normal (lilToon)", icon='PLUGIN')
+
+def register():
+    for c in classes:
+        bpy.utils.register_class(c)
+    bpy.types.VIEW3D_MT_object.append(menu_fn)
+
+def unregister():
+    bpy.types.VIEW3D_MT_object.remove(menu_fn)
+    for c in classes:
+        bpy.utils.unregister_class(c)
---
+++ operators.py
@@ -0,0 +1,53 @@
+import bpy
+import mathutils
+
+class BNFLO_OT_bake_normal(bpy.types.Operator):
+    bl_idname = "bnflo.bake_normal"
+    bl_label = "Bake Normal (lilToon)"
+    bl_options = {"REGISTER", "UNDO"}
+    bl_description = "Bake normals to vertex colors for lilToon outline."
+
+    @classmethod
+    def poll(cls, context):
+        return (
+            context.mode == "OBJECT" and
+            len(context.selected_objects) == 2 and
+            context.selected_objects[0].type == "MESH" and
+            context.selected_objects[1].type == "MESH" and
+            bpy.context.active_object == context.selected_objects[0]
+        )
+
+    def execute(self, context):
+        obj0 = context.selected_objects[0]
+        obj1 = context.selected_objects[1]
+
+        if bpy.context.active_object == obj0:
+            bake_normal(obj0, obj1)
+
+        else:
+            bake_normal(obj1, obj0)
+
+        return {"FINISHED"}
+
+def bake_normal(obj, src):
+    obj.data.calc_tangents()
+
+    if not obj.data.vertex_colors:
+        obj.data.vertex_colors.new()
+
+    for polygon in obj.data.polygons:
+        for loop_index in polygon.loop_indices:
+            obj_loop = obj.data.loops[loop_index]
+
+            obj_tangent = obj_loop.tangent
+            obj_bitangent = obj_loop.bitangent
+            obj_normal = obj_loop.normal
+
+            src_normal = src.data.loops[loop_index].normal
+
+            obj.data.vertex_colors[0].data[loop_index].color = (
+                mathutils.Vector.dot(src_normal, obj_tangent) * 0.5 + 0.5,
+                mathutils.Vector.dot(src_normal, obj_bitangent) * 0.5 + 0.5,
+                mathutils.Vector.dot(src_normal, obj_normal) * 0.5 + 0.5,
+                1.0
+            )
